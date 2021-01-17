e60f1f44ed028efa2dd7c11471da6a3474736109
Erik Drozina <erikdro41@gmail.com>
2021-01-17 15:12:34 +0100

    cleaned up a bit

--- superior_tucana.mk
+++ superior_tucana.mk
@@ -4,20 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from tucana device
 $(call inherit-product, device/xiaomi/tucana/device.mk)
 
 # Inherit from Superior vendor
 $(call inherit-product, vendor/superior/config/common.mk)
 
+#Screen Dimensions
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+#Boot Animation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
-EXTRA_FOD_ANIMATIONS := true
-#TARGET_GAPPS_ARCH := arm64
 
-# Launcher
-TARGET_LAUNCHER := oplauncher
+#Extra FOD Animations
+#EXTRA_FOD_ANIMATIONS := true
+
+#GApps
+#TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := superior_tucana
@@ -27,3 +31,4 @@ PRODUCT_MODEL := Mi Note 10
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
