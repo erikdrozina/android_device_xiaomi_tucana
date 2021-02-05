32562e8ae2afe732c58b88ccfa3b5d99efed396d
Erik Drozina <erikdro41@gmail.com>
2021-02-05 12:27:07 +0100

    Adapted to A11 vendor

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/superior_tucana.mk
+
+COMMON_LUNCH_CHOICES := \
+    superior_tucana-user \
+    superior_tucana-userdebug \
+    superior_tucana-eng
--- system_prop.mk
+++ system_prop.mk
@@ -4,7 +4,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
     persist.vendor.btstack.enable.splita2dp=true \
     persist.vendor.btstack.enable.twsplus=true \
+    persist.vendor.btstack.connect.peer_earbud=true \
     persist.vendor.btstack.enable.twsplussho=true \
+    persist.vendor.bt.a2dp.mac_whitelist=false \
+    persist.vendor.btsatck.absvolfeature=true \
     ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.bluetooth.soc=cherokee
 
@@ -16,13 +19,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.lux.d=50 \
     persist.vendor.camera.enableNCSService=TRUE \
     persist.vendor.camera.enableTOFInterface=TRUE \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
-    camera.disable_zsl_mode=1
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
+
+# System property determining camera HAL to be used for a Video call
+# 1 is camera1
+# 2 or anything else is camera2
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.VT_CAM_INTERFACE=1
 
 # CNE and DPM
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1 \
-    persist.vendor.dpm.feature=1 \
+    persist.vendor.dpm.feature=11 \
     persist.vendor.dpm.loglevel=0 \
     persist.vendor.dpm.nsrm.bkg.evt=3955
 
@@ -31,6 +39,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapsize=36m \
     dev.pm.dyn_samplingrate=1
     
+# Data storage improvement
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.use_fifo_ui=1
+
 # Data Modules
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.data.mode=concurrent \
@@ -50,7 +62,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.xiaomi.bl.poll=true \
     persist.displayfeature.dc_backlight.threshold=610 \
     persist.displayfeature.dc_backlight.enable=false \
-    persist.fod.modified.dc_status=false \
+    persist.fod.modified.dc_status=true \
     sys.displayfeature.hbm.enable=true
 
 
@@ -61,12 +73,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Gboard
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.ime.kb_pad_port_b=1
-    
+
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
-     debug.sf.enable_hwc_vds=1 \
+    debug.sf.enable_hwc_vds=1 \
     debug.cpurend.vsync=false
 
 # IOP
@@ -76,9 +88,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
+    audio.deep_buffer.media=true \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-scan=true \
     media.stagefright.thumbnail.prefer_hw_codecs=true \
-    media.settings.xml=/system/etc/media_profiles_vendor.xml \
-    vendor.mm.enable.qcom_parser=63963135
+    mmp.enable.3g2=true \
+    media.aac_51_output_enabled=true \
+    mm.enable.smoothstreaming=true \
+    media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
+    vendor.mm.enable.qcom_parser=67108863
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -86,29 +108,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Netflix
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.netflix.bsp_rev=Q855-16947-1
+    ro.netflix.bsp_rev=Q6150-17263-1
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    DEVICE_PROVISIONED=1 \
-    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
-    ril.subscription.types=RUIM \
-    ro.telephony.default_cdma_sub=0 \
-    ro.telephony.default_network=22,22 \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1 \
-    persist.radio.NO_STAPA=1 \
-    persist.radio.VT_HYBRID_ENABLE=1 \
-    persist.vendor.data.iwlan.enable=true \
-    persist.vendor.ims.disableADBLogs=1 \
-    persist.vendor.ims.disableIMSLogs=1 \
-    persist.vendor.radio.enable_temp_dds=true \
-    persist.vendor.radio.force_on_dc=true \
+    persist.radio.multisim.config=dsds \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.data_con_rprt=1 \
+    persist.vendor.radio.data_ltd_sys_ind=1 \
+    persist.vendor.radio.force_ltd_sys_ind=1 \
+    persist.vendor.radio.manual_nw_rej_ct=1 \
+    persist.vendor.radio.mt_sms_ack=30 \
+    persist.vendor.radio.procedure_bytes=SKIP \
+    persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.redir_party_num=1 \
-    persist.vendor.radio.report_codec=1 \
-    telephony.lteOnCdmaDevice=1
-
+    persist.vendor.radio.sib16_support=1 \
+    ro.telephony.default_cdma_sub=0 \
+    ro.telephony.default_network=22,22 
+    
 # RCS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.rcs.supported=1
@@ -126,7 +144,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
-    
+
 # USB debugging
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp,adb \
@@ -140,8 +158,14 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Vendor
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.va_aosp.support=1 
-
+    ro.vendor.qti.va_aosp.support=1
+    
+# WLAN for Android Q version
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.wlan.vendor=qcom \
+    ro.hardware.wlan.chip=39xx \
+    ro.hardware.wlan.mimo=1 
+    
 # Zygote
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
